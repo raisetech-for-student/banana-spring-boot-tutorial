dd39486cc3dce3551882bd6ee560fc2bc3de7adb
yassan-s <yasuro.2103@gmail.com>
2022-03-20 21:29:57 +0900

    first commit

---
+++ src/main/java/com/RESTAPI/CRUDapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.RESTAPI.CRUDapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApiTutorialApplication.class);
+	}
+
+}
