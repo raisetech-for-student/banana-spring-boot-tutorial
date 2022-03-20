122e0fe9e3a5da9340e0ff6634f46adff22901fc
yassan-s <yasuro.2103@gmail.com>
2022-03-20 23:55:51 +0900

    Project名を修正

--- src/main/java/com/RESTAPI/CRUDapp/ServletInitializer.java
+++ src/main/java/com/RESTAPI/CRUDapp/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RestApiTutorialApplication.class);
+		return application.sources(bananaSpringBootTutorialApplication.class);
 	}
 
 }
--- src/main/java/com/RESTAPI/CRUDapp/bananaSpringBootTutorialApplication.java
+++ src/main/java/com/RESTAPI/CRUDapp/bananaSpringBootTutorialApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiTutorialApplication {
+public class bananaSpringBootTutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiTutorialApplication.class, args);
+		SpringApplication.run(bananaSpringBootTutorialApplication.class, args);
 	}
 
 }
